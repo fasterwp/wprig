cc48f7e0285c41895e1dc9a0456c04eedd700032
Andrei Chira <andreichira777@gmail.com>
2018-09-12 22:43:08 +0300

    naming

--- dev/config/themeConfig.js
+++ dev/config/themeConfig.js
@@ -2,14 +2,14 @@
 
 module.exports = {
 	theme: {
-		slug: 'wprig',
-		name: 'WP Rig',
-		author: 'Morten Rand-Hendriksen'
+		slug: 'blogrise',
+		name: 'Blogrise',
+		author: 'Andrei Chira'
 	},
 	dev: {
 		browserSync: {
 			live: true,
-			proxyURL: 'wprig.test:8888',
+			proxyURL: 'wprig.local',
 			bypassPort: '8181'
 		},
 		browserslist: [ // See https://github.com/browserslist/browserslist
